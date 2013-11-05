2b097ce22fe702c0bc0a4a21f2215e735ea60349
Leandro Ostera <me@leostera.com>
2013-11-05 09:37:41 -0300

    Fixes directive for backgrounds.

--- modules/website/directives/backgrounds.js
+++ modules/website/directives/backgrounds.js
@@ -11,26 +11,32 @@ angular.module('website')
       priority: 0,
       restrict: 'E',
       template: ''
-        +'<img ng-repeat="bg in backgrounds" src="{{bg.url}}" class="intro" ng-class="{\'active\': bg.id===$parent.active}">'
+        +'<img ng-repeat="bg in backgrounds" src="{{bg.url}}" ng-if="bg.id===$parent.active">'
       , link: function(scope, element, attr) {
         scope.backgrounds = [1,2,3,4,5,6,7,8,9].map(function (id) {
           return {url: '/build/website/images/'+id+'.jpg', id: id};
         });
 
         function newActive (time) {
-          time = time || 0;
           $timeout(function () {
-            scope.active = Math.floor((Math.random()*scope.backgrounds.length))+1
-            scope.active = scope.active%scope.backgrounds.length;
-            console.log("new image", scope.active);
-          }, time);
+            time = time || 500;
+            last = scope.active;
+            console.log("Fading...", scope.active);
+            scope.active = -1;
+            $timeout(function () {
+              do {
+                scope.active = Math.floor((Math.random()*scope.backgrounds.length))+1
+                scope.active = scope.active%scope.backgrounds.length+1;
+              } while (last === scope.active);
+              console.log("Showing...", scope.active);
+            }, time);
+          });
         }
 
-        newActive(1000);
+        newActive();
 
-        setInterval(function () {
-          newActive();  
-        },10000);
+        setInterval(newActive, 5000);
+
       }
     }
   }
